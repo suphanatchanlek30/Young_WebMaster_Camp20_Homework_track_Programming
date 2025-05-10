46f4800439ed6b45d3dbd3b17ac792a3ab443642
suphanatchanlek <suphanatchanlek@gmail.com>
2025-05-11 00:18:02 +0700

    update text center

--- src/app/details/page.js
+++ src/app/details/page.js
@@ -1,6 +1,6 @@
 export default function DetailsPage() {
   return (
-    <div className={`p-8 text-[20px] bg-[#190200] min-h-screen justify-center items-center flex flex-col gap-4`}>
+    <div className={`p-8 text-[20px] bg-[#190200] min-h-screen justify-center items-center text-center flex flex-col gap-4`}>
       <h1 className={`text-2xl font-bold text-[#ffffff]`}>รายละเอียดของผู้ที่ผ่านการคัดเลือก</h1>
       <p className={`text-[#ffffff]`}>ยินดีต้อนรับสู่หน้ารายละเอียด 🎉</p>
     </div>
