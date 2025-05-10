09bad33cd529f097f0e88f7af9b7b0be164deffb
suphanatchanlek <suphanatchanlek@gmail.com>
2025-05-10 15:33:29 +0700

    update

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Logo from "../../../public/Logo_Navbar/logo-ywc20-mono.png";
 import DiscordButton from "./DiscordButton";
+import Link from "next/link";
 
 export default function Navbar() {
   const scrollToTop = () => {
@@ -11,19 +12,22 @@ export default function Navbar() {
     <nav className={`bg-black fixed w-full z-40`}>
       <div className={`flex items-center justify-between xl:container mx-auto transition-all px-8 py-4`}>
         <div className={`flex justify-between items-center gap-8`}>
-          <a href="/">
-            <Image src={Logo} alt="Logo" width={64} height={32} priority />
-          </a>
+          {/* Use Link for internal navigation */}
+          <Link href="/" passHref>
+            <a>
+              <Image src={Logo} alt="Logo" width={64} height={32} priority />
+            </a>
+          </Link>
 
           <div className={`lg:flex hidden gap-8 text-white font-sans-serif text-[16px]`}>
-            <span onClick={scrollToTop} style={{ cursor: 'pointer' }}>Home</span> {/* ใช้ span และ onClick */}
-            
+            {/* Scroll-to-top functionality */}
+            <span onClick={scrollToTop} style={{ cursor: 'pointer' }}>Home</span>
           </div>
         </div>
 
         <div className={`flex items-center gap-4`}>
           <DiscordButton>
-            <a href="https://discord.gg/zjuU34Kh" target="_blank" rel=""></a>
+            <a href="https://discord.gg/zjuU34Kh" target="_blank">Join Discord</a>
           </DiscordButton>
         </div>
       </div>
